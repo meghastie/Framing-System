5520c86d781ac756ae3f67f4906700d35f51cdc9
tjb22146 <megan.hastie.2022@uni.strath.ac.uk>
2024-10-09 15:54:31 +0100

    Amend Stage A

--- frame.php
+++ frame.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Frame Price Estimator</title>
-</head>
-<body>
-
-
-<h1>Frame Price Estimator</h1>
-<p>Please enter your photo sizes to get a framing cost estimate</p>
 
 <?php
 $width = isset($_POST['width'])? $_POST['width'] : " ";
@@ -22,13 +11,5 @@ if($width !== " " && $height !== " "){
 }
 ?>
 
-<form action="" method="post">
-    Photo Width: <input type="number" name="width"><br>
-    Photo Height: <input type="number" name="height"><br>
-    <input type="submit">
-</form>
-
-</body>
-</html>
 
 
