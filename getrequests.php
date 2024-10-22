e2a6a81896a6955f9c07b015a4faa4adb3010b06
tjb22146 <megan.hastie.2022@uni.strath.ac.uk>
2024-10-22 12:28:08 +0100

    Stage E complete

--- getrequests.php
+++ getrequests.php
@@ -1,8 +1,11 @@
 
+<title>Frame Price Estimator Requests</title>
+
 
 <?php
-$formPassword = isset($_POST['password'])? $_POST['password'] : "";
+$formPassword = isset($_POST['password'])? $_POST['password'] : '';
 
+//connect to our database
 $host = "devweb2024.cis.strath.ac.uk";
 $username = "tjb22146";
 $password = "Riu2othacaec";
@@ -13,12 +16,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 
 }
+
+//get all orders to display
 $sql = "SELECT * FROM `tjb22146`.`framingSystem`";
 $result = $conn->query($sql);
 if ($result === FALSE) {
     die ("Error: " . $sql . "<br>" . $conn->error);//FIXME only use during debugging
 }
 
+//display table if the correct password is entered
 if($formPassword == "WinTheGameNow"){
     echo" <table style = 'width: 1000px'>";
         echo"<tr>
@@ -30,21 +36,40 @@ if($formPassword == "WinTheGameNow"){
             <th>Requested</th>
         </tr>";
         while ($row = $result->fetch_assoc()) {
+
+            //calculate if our order is overdue using DateTime class
+            $requestedDate = DateTime::createFromFormat('d/m/Y - H:i', $row['Requested']);
+            $currentDate = new DateTime();
+
+            $interval = $requestedDate->diff($currentDate);
+            $daysInterval = $interval->d;
+            $overdue = false;
+
+            $postageType = $row['Postage'];
+
+            if(($postageType == 'Economy' && $daysInterval > 7) || ($postageType == 'Rapid' && $daysInterval > 3) || ($postageType == 'Next Day' && $daysInterval > 1) ){
+                $overdue = true;
+            }
+
             echo "<tr>";
             foreach ($row as $data) {
-                echo "<td  style = 'width: 166px; text-align: center'>" . htmlspecialchars($data) . "</td>";
+                if($overdue){
+                    echo "<td  style = 'width: 166px; text-align: center; font-weight:bold'>" . htmlspecialchars($data) . "</td>";
+                }else{
+                    echo "<td  style = 'width: 166px; text-align: center'>" . htmlspecialchars($data) . "</td>";
+                }
+
             }
             echo "</tr>";
         }
     echo "</table>";
 
-//Add a new page - getrequests.php that lists the entries in the database as a simple HTML table. Initially the page should only show a password field and submit button. On submission back to getrequests.php it should show the table if the password is correct. A single password of WinTheGameNow should be used (normally bad practice!).
-//
-//Show in bold any orders that are overdue (over 1 day old for next-day, over 3 days old for rapid and over 7 days old for economy postage).
-}else{
-    echo"Wrong password. Please try again";
+}else{ //if the wrong password is entered, show form back to user
+    if($_SERVER["REQUEST_METHOD"] == "POST"){ //only send error message IF the form has been submitted
+        echo "Wrong password. Please try again.";
+    }
     ?>
-    <form action="getrequests.php" method="post">
+    <form action="" method="post">
         <input type="text" name="password" value = "<?php echo $formPassword?>">
         <input type="submit">
     </form>
